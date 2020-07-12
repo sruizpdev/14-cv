5eef007bdf419841b56f52ae716b45db815a4ffe
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-12 19:50:10 +0200

    instalando la fuente de datos DatoCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `573a6702c89c9cf096b47e75291f45`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
