7687333f46f41c464f76a23d944bad98162a4da1
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-12 17:40:30 +0200

    esqueleto del header component + helmet

---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React from "react"
+const Header = () => {
+  return <h1>desde el header</h1>
+}
+
+export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,25 @@
 import React from "react"
+import Header from "./header"
+import { Helmet } from "react-helmet"
+
 const Layout = props => {
-  return <>{props.children}</>
+  return (
+    <>
+      <Helmet>
+        <title>Sergio Ruiz Pérez - CV</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Header />
+      {props.children}
+    </>
+  )
 }
 
 export default Layout
