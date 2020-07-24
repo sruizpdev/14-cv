83c0efdc0bfb0d62bf57ae4cc7a2e1337a782444
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-24 14:17:16 +0200

    simplificando skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import DrawSkill from "./draw-skill"
+import Image from "gatsby-image"
+import { css } from "@emotion/core"
+import styled from "@emotion/styled"
 
 const Skills = () => {
   const skills = useStaticQuery(graphql`
@@ -11,6 +13,11 @@ const Skills = () => {
             skill {
               skillName
               skillKnowledge
+              logo {
+                fluid {
+                  ...GatsbyDatoCmsFluid
+                }
+              }
             }
           }
         }
@@ -20,11 +27,75 @@ const Skills = () => {
 
   const array_skills = skills.allDatoCmsSkill.edges[0].node.skill
 
+  const Knowledge = styled.div`
+    width: 100%;
+    height: 30px;
+    background-color: #f5f5f5;
+  `
+  const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 20px 0;
+    align-items: center;
+  `
+  const ColLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+    padding-left: 30px;
+  `
+  const ColRight = styled.div`
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 11;
+    padding-right: 30px;
+  `
+  const Container = styled.div`
+    margin-top: 30px;
+  `
+
   return (
     <>
-      {array_skills.map(skill => (
-        <DrawSkill key={skill.skillName} skill={skill} />
-      ))}
+      <Container>
+        {array_skills.map(skill => {
+          const { skillName, skillKnowledge } = skill
+          return (
+            <>
+              <Row>
+                <ColLeft>
+                  <Image
+                    css={css`
+                      width: 40px;
+                    `}
+                    fluid={skill.logo.fluid}
+                    alt={skill.skill_name}
+                  />
+                </ColLeft>
+                <ColRight>
+                  <Knowledge>
+                    <div
+                      css={css`
+                        display: flex;
+                        align-items: center;
+                        padding-left: 10px;
+                        width: ${skillKnowledge}%;
+                        background-color: #337ab7;
+                        height: 30px;
+                        color: white;
+                      `}
+                    >
+                      {skillName} - {skillKnowledge}%
+                    </div>
+                  </Knowledge>
+                </ColRight>
+              </Row>
+            </>
+          )
+        })}
+      </Container>
     </>
   )
 }
