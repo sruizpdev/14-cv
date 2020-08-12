804c42b05c9100541771f37d60b18751fd8b03ce
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-08-12 14:10:05 +0200

    cv link

--- src/components/attachments.js
+++ src/components/attachments.js
@@ -16,7 +16,7 @@ const Attachments = () => {
             color: black;
             text-decoration: none;
           `}
-          href="https://www.datocms-assets.com/31437/1596389111-cvsergioruizen.pdf"
+          href="https://www.datocms-assets.com/31437/1597234065-sergioruizcv.pdf"
           target="_blank"
         >
           Curriculum-Vitae.pdf
