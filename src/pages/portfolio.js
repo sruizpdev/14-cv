499d8eee1d5c4fc01c5d12be673969ea99a63e77
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-08-12 13:42:40 +0200

    correccion ortografica works

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import GeneralInfo from "../components/general-info"
-import Works from "../components/Works"
+import Works from "../components/works"
 
 const PortfolioPage = () => (
   <Layout>
